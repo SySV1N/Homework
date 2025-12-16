f674441b7416168cf0e9de2d23e9d8f76573432a
SySV1N <system3224@gmail.com>
2025-12-16 22:53:23 +0300

    Fixed var

--- Lesson3/index.js
+++ Lesson3/index.js
@@ -2,7 +2,7 @@ const pinCodes = ['1111', '5678', '1234', '9999'];
 const myPin = '1234';
 
 for (const pinCode of pinCodes) {
-    if (pinCode === '1234') {
+    if (pinCode === 'myPin') {
         console.log(`Пин-код найден!`);
         break;
     }
