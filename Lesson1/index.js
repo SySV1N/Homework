93dab75053a02a3c4f2fa0f42959ec9620ebe5c0
SySV1N <system3224@gmail.com>
2025-12-16 22:34:08 +0300

    Changed positions

--- Lesson1/index.js
+++ Lesson1/index.js
@@ -1,15 +1,1 @@
-const totalPrice = 5000;
-let accountBalance = 4500;
-const isVip = true;
-if (accountBalance >= totalPrice || (isVip && accountBalance >= totalPrice * 0.9)) {
-    if(!isVip) {
-    accountBalance -= totalPrice;
-    console.log(`Оставшийся баланс: ${accountBalance}`);
-    }
-    else {
-        accountBalance -= totalPrice * 0.9;
-        console.log(`Оставшийся баланс: ${accountBalance}`);
-    }
-} else {
-    console.log('Недостаточно средств');
-}
+console.log('Меня зовут Глеб');
