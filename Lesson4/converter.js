39d7d8801df7dda264ebefa8d12b08284dfcab54
SySV1N <system3224@gmail.com>
2025-12-18 23:32:31 +0300

    Added description

--- Lesson4/converter.js
+++ Lesson4/converter.js
@@ -1,14 +1,14 @@
-const usdCourse = 90; 
+const usdCourse = 90; // Задал курс валют
 const euroCourse = 100;
 
-const product = (a, b) => a * b;
+const product = (a, b) => a * b; // Для практики ввел функцию произведения
 
-const usdToRub = (usd) => {
+const usdToRub = (usd) => { // Функция конвертации доллара
     return (` ${usd}$ = ${product(usdCourse, usd)} руб.`);
 }
 
-const euroToRub = (euro) => {
+const euroToRub = (euro) => { // Функция конвертации евро
     return (`${euro}€ = ${product(euroCourse, euro)} руб.`);
 }
 
-console.log(usdToRub(10),'\n', euroToRub(20));
+console.log(usdToRub(10),'\n', euroToRub(20)); // Вывод калькуляций
