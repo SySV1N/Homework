a5ef6b98df8f0967ada4ad68ee051d6bfb1f6f5b
SySV1N <system3224@gmail.com>
2026-01-09 17:38:22 +0300

    Added styles

--- Lesson7/index.js
+++ Lesson7/index.js
@@ -3,21 +3,25 @@ const PORT = 3000; //Задал порт
 const fs = require("node:fs/promises"); // Импортировал версию file system с промисами
 const path = require("node:path"); // Импортировал модуль пути, чтобы код работал на всех ОС
 
-const sendFile = async(fileName, response, statusCode) => { //Создал функцию для разных ссылок
-    const fullPath = path.join(__dirname, "public", `${fileName}.html`);
+const sendFile = async(fileName, type, response, statusCode) => { //Создал функцию для разных ссылок
+    response.setHeader('Content-Type', `text/${type}; charset=utf-8`);
+
+    const fullPath = path.join(__dirname, "public", `${fileName}.${type}`);
     const page = await fs.readFile(fullPath, 'utf-8');
     response.statusCode = statusCode;   
     response.end(page);
 };
 const server = http.createServer(async (request, response) => {
-    response.setHeader('Content-Type', 'text/html; charset=utf-8');
+    //response.setHeader('Content-Type', 'text/html; charset=utf-8');
     
     if (request.url === '/') {
-        await sendFile("index", response, 200);
+        await sendFile("index", "html", response, 200);
     } else if (request.url === '/rules') {
-        await sendFile("rules", response, 200);
+        await sendFile("rules", "html", response, 200);
+    } else if (request.url === '/style.css') {
+        await sendFile("style", "css", response, 200);
     } else {
-        await sendFile("404", response, 404);
+        await sendFile("404", "html", response, 404);
     };
 });
 
